55f67af97d7a574111aaa096c5868d1ad38ae16c
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2020-07-22 18:31:28 -0300

    Classe de util para Strings

---
+++ src/resources/bibliotecas/StringUtils.java
@@ -0,0 +1,23 @@
+package resources.bibliotecas;
+
+import java.nio.charset.Charset;
+
+public class StringUtils {
+    // SUPORTE PARA UTF-8 no area console
+    private static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+    
+    private StringUtils() {
+	// TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * Retorna a string no formato UTF-8
+     * @param string - string a ser convertida
+     * @return - string convertida para utf-8
+     */
+    public static String getStringUtf8(String string) {
+	  byte[] ptext = string.getBytes(ISO_8859_1);
+	  return  new String(ptext, UTF_8);
+    }
+}
