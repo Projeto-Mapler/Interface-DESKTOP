c64a184179e514df0f9125758872aab3ede34c2d
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-07-24 09:44:56 -0300

    Update AlertaService.java

--- src/main/java/mapler/service/AlertaService.java
+++ src/main/java/mapler/service/AlertaService.java
@@ -12,42 +12,45 @@ import mapler.util.CarregadorRecursos;
  * Exibe alertas da IDE
  */
 public final class AlertaService {
-  private static final String ESTILO_CSS = CarregadorRecursos.getResourceExternalForm(Estilos.ARLETAS.getUrl());
-  /**
-   * Classe estática
-   */
-  private AlertaService() {}
+	private static final String ESTILO_CSS = CarregadorRecursos.getResourceExternalForm(Estilos.ARLETAS.getUrl());
 
-  public static void showAviso(String aviso) {
-    Alert dialogoInfo = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.CLOSE);
-    DialogPane dialogPane = dialogoInfo.getDialogPane();
-    dialogPane.getStylesheets().add(CarregadorRecursos.getResourceExternalForm(Estilos.ARLETAS.getUrl()));
-    dialogPane.getStyleClass().add("myDialog");
-    dialogoInfo.setTitle("AVISO");
-    dialogoInfo.setHeaderText(aviso);
-    dialogoInfo.setContentText(" ");
-    dialogoInfo.showAndWait();
-  }
+	/**
+	 * Classe estática
+	 */
+	private AlertaService() {
+	}
 
-  public static int showConfirm(String aviso) {
-    Alert dialogoInfo = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.CLOSE);
-    DialogPane dialogPane = dialogoInfo.getDialogPane();
-    dialogPane.getStylesheets().add(CarregadorRecursos.getResourceExternalForm(Estilos.ARLETAS.getUrl()));
-    dialogPane.getStyleClass().add("myDialog");
-    dialogoInfo.setTitle("CONFIRMAÇÃO");
-    dialogoInfo.setHeaderText(aviso);
-    dialogoInfo.setContentText(" ");
-    ButtonType btnSim = new ButtonType("Sim");
-    ButtonType btnNao = new ButtonType("Não");
-    ButtonType btnCancelar = new ButtonType("Cancelar", ButtonBar.ButtonData.CANCEL_CLOSE);
-    dialogoInfo.getButtonTypes().setAll(btnSim, btnNao, btnCancelar);
-    Optional<ButtonType> result = dialogoInfo.showAndWait();
-    if (result.isPresent() && result.get() == btnSim) {
-      return 1;
-    } else if (result.isPresent() && result.get() == btnNao) {
-      return 0;
-    } else {
-      return -1;
-    }
-  }
+	private static Alert criarJanela(Alert.AlertType alertType, String aviso, String titulo) {
+		Alert dialogoInfo = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.CLOSE);
+		DialogPane dialogPane = dialogoInfo.getDialogPane();
+		dialogPane.getStylesheets().add(CarregadorRecursos.getResourceExternalForm(Estilos.ARLETAS.getUrl()));
+		dialogPane.getStyleClass().add("myDialog");
+		dialogoInfo.setTitle(titulo);
+		dialogoInfo.setHeaderText(aviso);
+		dialogoInfo.setContentText(" ");
+
+		return dialogoInfo;
+	}
+
+	public static void showAviso(String aviso) {
+		criarJanela(Alert.AlertType.INFORMATION, aviso, "Aviso").showAndWait();
+	}
+
+	public static int showConfirm(String aviso) {
+		Alert dialogoInfo = criarJanela(Alert.AlertType.CONFIRMATION, aviso, "Confirmação");
+		ButtonType btnSim = new ButtonType("Sim");
+		ButtonType btnNao = new ButtonType("Não");
+		ButtonType btnCancelar = new ButtonType("Cancelar", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+		dialogoInfo.getButtonTypes().setAll(btnSim, btnNao, btnCancelar);
+		Optional<ButtonType> result = dialogoInfo.showAndWait();
+
+		if (result.isPresent() && result.get() == btnSim) {
+			return 1;
+		} else if (result.isPresent() && result.get() == btnNao) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
 }
