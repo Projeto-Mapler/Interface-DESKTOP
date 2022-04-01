088111cfbf14fc89690fe974a23a315a323b0bc7
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-31 22:13:53 -0300

    Create Legenda.java

---
+++ src/main/java/mapler/fluxograma/figuras/Legenda.java
@@ -0,0 +1,33 @@
+package mapler.fluxograma.figuras;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import mapler.model.resource.Templates;
+
+public class Legenda extends Figura{
+
+	public AnchorPane criar_legenda() {
+		  AnchorPane legenda;
+		  try {
+			  legenda = FXMLLoader.load(getClass().getResource(Templates.FIG_FIM.getUrl()));
+		  }catch(Exception e) {
+			  legenda = new AnchorPane();
+			  System.out.println(e.getMessage());
+		  }
+		  legenda.setId("#legenda"+SequenceIdFigura.getNextID());
+		  return legenda;
+	  }
+	
+	public AnchorPane criar_legenda(String id) {
+		  AnchorPane legenda;
+		  try {
+			  legenda = FXMLLoader.load(getClass().getResource(Templates.FIG_FIM.getUrl()));
+		  }catch(Exception e) {
+			  legenda = new AnchorPane();
+			  System.out.println(e.getMessage());
+		  }
+		  legenda.setId(id);
+		  return legenda;
+	  }
+	
+}
